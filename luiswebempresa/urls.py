c112d901ac76f8bbf7b04c70614a5d1648864367
Luis Angel Larios Castillo <luis44larios.99@gmail.com>
2020-05-22 17:49:50 -0500

    Se crean la app para pages y modifica sample.html

--- luiswebempresa/urls.py
+++ luiswebempresa/urls.py
@@ -22,8 +22,11 @@ urlpatterns = [
     path('', include('core.urls')), 
     #Paths del services
     path('services/', include('services.urls')), 
-    #Paths del services
+    #Paths del blog
     path('blog/', include('blog.urls')), 
+
+    #Paths del pages
+    path('page/', include('pages.urls')), 
     #Paths del admin
     path('admin/', admin.site.urls),
 ]
