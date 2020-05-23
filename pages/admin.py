ebbcb43abae5820429110e6a02e7ca9f174b6c10
Luis Angel Larios Castillo <luis44larios.99@gmail.com>
2020-05-22 19:38:38 -0500

    Se crea fichero templatetags y se configuran las entradas con  ckeditor

--- pages/admin.py
+++ pages/admin.py
@@ -5,5 +5,6 @@ from .models import Page
 
 class PageAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
+    list_display = ('title', 'order')
 
 admin.site.register(Page, PageAdmin)
